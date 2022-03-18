2e154b2bcb4e38fde45474eeaed1849aeb6431dc
João Spinelli <jgsspinelli92@gmail.com>
2022-03-18 14:24:36 -0300

    Update dollyEnglishTeacher.js

--- kata6/dollyEnglishTeacher.js
+++ kata6/dollyEnglishTeacher.js
@@ -3,7 +3,7 @@
   pangram é uma frase que contem todas as letras do alfabeto pelo menos uma vez.
   por exemplo a frase: "The quick brown fox jumps over the lazy dog" é um pangram, pq usam as letras A - Z pelo menos uma vez(não é para diferenciar maiuscula de minuscula).
 
-  dada uma string devina se é ou não um pangram, retornando true quando for e false quando não for.
+  dada uma string defina se é ou não um pangram, retornando true quando for e false quando não for.
 */
 
 function isPangram(phrase) {
