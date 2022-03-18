26df95c1231e99b59e85cdb008132ecdb4057a2e
João Spinelli <jgsspinelli92@gmail.com>
2022-03-18 14:31:11 -0300

    Update captainDolly.js

--- kata8/captainDolly.js
+++ kata8/captainDolly.js
@@ -6,7 +6,6 @@
     Você tem canhões de batalha, eles estão prontos?
     Seu trabalho é checar se os atiradores estão carregados e prontos, se eles estiverem: Atire!
     Se eles não estiverem prontos:  Tragam-me polvora!
-    A quantidade de atiradores para cada caso de teste vão ser de 2, 3 ou 4.
     Quando você fizer a pergunta a resposta deles deve ser: aye ou nay.
     Obs: Atirar com menos de todos os canhões prontos não valem (Isso não é atire a vontade e sim atirar só quando o capitão Dolly avisar, seus cães sarnentos imundos!).
     Se todas as respostas forem 'aye' então 'Atire!' se uma ou mais forem 'nay' então 'Tragam-me polvora'.
